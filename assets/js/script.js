66a1f8b7ab266dcfbed3fce3e9117a58a1adc160
mythos86 <46715182+mythos86@users.noreply.github.com>
2025-09-04 21:41:41 +0200

    Update script.js
    positie personal message

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ document.getElementById('generate').addEventListener('click', function() {
     const celebrant = document.getElementById('celebrant').value.trim();
     const wisher = document.getElementById('wisher').value.trim();
     const personalWish = document.getElementById('personalWish').value.trim();
-
+    const position = document.getElementById('position').value;
+    
     if (celebrant && wisher) {
         // Prepare the URL for the birthday page with query parameters
         const url = `birthday.html?celebrant=${encodeURIComponent(celebrant)}&wisher=${encodeURIComponent(wisher)}&personalWish=${encodeURIComponent(personalWish)}`;
