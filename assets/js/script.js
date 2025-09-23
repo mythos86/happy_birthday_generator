2b3dfd132c4d1f0e5155d1fd522369449b7a509a
JackyKeibeckPXL <jacky.keibeck@student.pxl.be>
2025-09-23 23:12:21 +0200

    update page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,13 @@ document.getElementById('generate').addEventListener('click', function() {
     const personalWish = document.getElementById('personalWish').value.trim();
     const position = document.getElementById('position').value;
 
-     if (celebrant && wisher) {
-        // URL veilig maken
+    if (celebrant && wisher) {
         const encodedCelebrant = encodeURIComponent(celebrant);
         const encodedWisher = encodeURIComponent(wisher);
         const encodedPersonalWish = encodeURIComponent(personalWish);
         const encodedPosition = encodeURIComponent(position);
 
-        // URL samenstellen met gecodeerde waarden
         const url = `birthday.html?celebrant=${encodedCelebrant}&wisher=${encodedWisher}&personalWish=${encodedPersonalWish}&position=${encodedPosition}`;
-        
-        // Doorsturen naar de birthday pagina
         window.location.href = url;
     } else {
         alert('Vul zowel de naam van jarige als jouw naam in!');
